ca35e44ab65d19196d5739b4f3cd3cc90672065d
Rodrez <fabian.rodrez@gmail.com>
2022-10-02 21:13:30 -0500

    Minor updates

--- CS-320/FirstMilestone/src/Appointment/Appointment.java
+++ CS-320/FirstMilestone/src/Appointment/Appointment.java
@@ -32,3 +32,23 @@ public class Appointment {
         }
         this.description = description;
     }
+
+    protected void setUniqueId() {
+        String uuid = UUID.randomUUID().toString(); // Set appointment ID
+        this.id = uuid.substring(uuid.length() - 16);
+    }
+
+    // Simplify the code above by using the constructor low
+    public Appointment(String date, String description) {
+        setUniqueId();
+        setDate(date);
+        setDescription(description);
+
+        System.out.printf("%s", this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment: ID=" + id + ", date=" + date + ", description=" + description;
+    }
+}
